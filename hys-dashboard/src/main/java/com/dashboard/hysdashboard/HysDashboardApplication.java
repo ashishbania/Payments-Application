5960f5ebea0f7f526b837ab9ce9015cdbd64f606
ashishbania <60585651+ashishbania@users.noreply.github.com>
2023-04-23 18:14:56 -0400

    added docker file for accounts-api

--- hys-dashboard/src/main/java/com/dashboard/hysdashboard/HysDashboardApplication.java
+++ hys-dashboard/src/main/java/com/dashboard/hysdashboard/HysDashboardApplication.java
@@ -3,15 +3,14 @@ package com.dashboard.hysdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.*
+import org.springframework.cloud.netflix.hystrix.dashboard.*;
+
 @SpringBootApplication
 @EnableEurekaServer
 @EnableHystrixDasboard
 public class HysDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HysDashboardApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HysDashboardApplication.class, args);
+  }
 }
